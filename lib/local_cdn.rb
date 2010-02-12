ae9968c81691916c8f1196d699e3cf4f8970a13d
alexreisner <alex@alexreisner.com>
2010-02-12 00:26:32 -0500

    Make Rails 3-compatible.

--- lib/local_cdn.rb
+++ lib/local_cdn.rb
@@ -4,41 +4,43 @@ module LocalCDN
 	# Render a stylesheet link tag for an array of YUI stylesheets. Uses local
 	# copy in development mode, Yahoo CDN otherwise. Takes an array of
 	# (relative) stylesheet URLs. For example:
-	#
+	# 
 	#   <%= yui_stylesheet_link_tag \
-  #       "2.7.0/build/reset-fonts-grids/reset-fonts-grids.css",
-  #       "2.7.0/build/base/base-min.css",
-  #       "2.7.0/build/container/assets/container-core.css",
-  #       "2.7.0/build/tabview/assets/tabview-core.css" %> 
+  #     "2.7.0/build/reset-fonts-grids/reset-fonts-grids.css",
+  #     "2.7.0/build/base/base-min.css",
+  #     "2.7.0/build/container/assets/container-core.css",
+  #     "2.7.0/build/tabview/assets/tabview-core.css" %> 
 	#
 	def yui_stylesheet_link_tag(*files)
-	  if RAILS_ENV == 'development'
+	  if Rails.env == "development"
 	    base_url = "http://localhost/yui/"
-	    files.map{ |f| stylesheet_link_tag(base_url + f) }.join("\n")
+	    out = files.map{ |f| stylesheet_link_tag(base_url + f) }.join("\n")
     else
       base_url = "http://yui.yahooapis.com/combo?"
-	    stylesheet_link_tag base_url + files.join('&')
+	    out = stylesheet_link_tag(base_url + files.join("&"))
     end
+    rails_3?? out.html_safe : out
 	end
 	
 	##
 	# Render a javascript include tag for an array of YUI scripts. Uses local
 	# copy in development mode, Yahoo CDN otherwise. Takes an array of
 	# (relative) script URLs. For example:
-	#
+	# 
   #   <%= yui_javascript_include_tag \
-  #       "2.7.0/build/yahoo-dom-event/yahoo-dom-event.js",
-  #       "2.7.0/build/dragdrop/dragdrop-min.js",
-  #       "2.7.0/build/container/container-min.js" %>
+  #     "2.7.0/build/yahoo-dom-event/yahoo-dom-event.js",
+  #     "2.7.0/build/dragdrop/dragdrop-min.js",
+  #     "2.7.0/build/container/container-min.js" %>
 	#
 	def yui_javascript_include_tag(*files)
-	  if RAILS_ENV == 'development'
+	  if Rails.env == "development"
 	    base_url = "http://localhost/yui/"
-	    files.map{ |f| javascript_include_tag(base_url + f) }.join("\n")
+	    out = files.map{ |f| javascript_include_tag(base_url + f) }.join("\n")
     else
       base_url = "http://yui.yahooapis.com/combo?"
-	    javascript_include_tag base_url + files.join('&')
+	    out = javascript_include_tag base_url + files.join("&")
     end
+    rails_3?? out.html_safe : out
 	end
 	
 	##
@@ -46,8 +48,9 @@ module LocalCDN
 	# Google CDN otherwise.
 	#
 	def prototype_include_tag
-	  javascript_include_tag (RAILS_ENV == 'development') ? "prototype" :
-      "http://ajax.googleapis.com/ajax/libs/prototype/1.6.0.3/prototype.js"
+	  out = javascript_include_tag(Rails.env == "development" ? "prototype" :
+      "http://ajax.googleapis.com/ajax/libs/prototype/1.6.0.3/prototype.js")
+    rails_3?? out.html_safe : out
 	end
   
 	##
@@ -55,11 +58,22 @@ module LocalCDN
 	# Google CDN otherwise.
 	#
 	def scriptaculous_include_tag(*files)
-	  unless RAILS_ENV == 'development'
+	  unless Rails.env == "development"
 	    files.map! do |f|
 	      "http://ajax.googleapis.com/ajax/libs/scriptaculous/1.8.1/#{f}.js"
 	    end
 	  end
-	  javascript_include_tag(*files)
+	  out = javascript_include_tag(*files)
+    rails_3?? out.html_safe : out
+	end
+	
+	
+	private # -----------------------------------------------------------------
+	
+	##
+	# Are we on Rails 3?
+	#
+	def rails_3?
+	  Rails.version[0...1] == "3"
 	end
 end
